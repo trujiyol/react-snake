a20934dc07bb300e935a92564a2b45cdc9e4ee02
Lucas Perez <lucasperezt@gmail.com>
2020-01-26 18:21:11 +0000

    Updates

---
+++ precache-manifest.7cb87deca3cda6b8f7877496e977d3de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ce708888d9589ee582f4a7921e101a",
+    "url": "/react-snake/index.html"
+  },
+  {
+    "revision": "cebc339409e930e66bec",
+    "url": "/react-snake/static/css/main.0e32be74.chunk.css"
+  },
+  {
+    "revision": "bcdab434eed77186fb8f",
+    "url": "/react-snake/static/js/2.2f92ff46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-snake/static/js/2.2f92ff46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cebc339409e930e66bec",
+    "url": "/react-snake/static/js/main.09a5b6d2.chunk.js"
+  },
+  {
+    "revision": "a076f48b92e69d48ea3c",
+    "url": "/react-snake/static/js/runtime-main.cd10f37a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-snake/static/media/logo.5d5d9eef.svg"
+  }
+]);
